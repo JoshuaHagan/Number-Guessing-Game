8dca090c55726891ffd7c09fd59de09a8766e2cc
joshuanathanielhagan <joshuanathanielhagan@gmail.com>
2018-08-29 17:59:48 -0500

    Add project files.

---
+++ Number Guessing Game/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+	int guess;
+	char input = 0;
+	int lessThan = 1;
+	int greaterThan = 2;
+	int equals = 3;
+	guess = rand() % 100 + 1;
+	std::cout << "Think of a number\n";
+	while(input != guess)
+	{
+		std::cout << "Enter a number\n";
+		std::cin >> input;
+		std::cout << "Is this your number " << guess << std::endl;
+		if (guess == input)
+		{
+			std::cout << 
+		}
+		
+
+	}
+	 
+
+
+
+
+	system("pause");
+}
